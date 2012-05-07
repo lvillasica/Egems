7c2d49851b04818354ead550f7053217875ea917
Marjun Pagalan <mpagalan@gmail.com>
2012-05-07 14:16:13 +0800

    project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Egems::Application.config.secret_token = '86cbe605b0be98be72cccf738df4794dc19da62743ef6c5b862a2f1625c9859a655ddfd20b949250c60c072f1f1faf5a918a7e5520ab93929122d16150c1d30b'
