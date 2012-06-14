1235049e414baedbe09d4122efb70fb08e9fd430
Leo Martin Villasica <exst_lvillasica@yahoo.com>
2012-06-14 15:37:55 +0800

    modified digiclock to display server time rather than client time.

--- app/assets/javascripts/digiclock.js
+++ app/assets/javascripts/digiclock.js
@@ -1,16 +1,17 @@
 $(function() {
+  var serverTime = new Date($("span#time").data('time'));
   var prependZero = function(num) {
     return (num < 10)? ("0" + num) : num;
   }
   
   setInterval(function() {
-    now = new Date();
-    h = now.getHours();
-    h_12hr = (h % 12);
-    hh = prependZero((h_12hr == 0)? 12 : h_12hr);
-    mm = prependZero(now.getMinutes());
-    ss = prependZero(now.getSeconds());
-    a = (h >= 12)? "pm" : "am";
+    serverTime.setSeconds(serverTime.getSeconds() + 1);
+    var h = serverTime.getHours(),
+        h_12hr = (h % 12),
+        hh = prependZero((h_12hr == 0)? 12 : h_12hr),
+        mm = prependZero(serverTime.getMinutes()),
+        ss = prependZero(serverTime.getSeconds()),
+        a = (h >= 12)? "pm" : "am";
     
     $('span#time').html(hh + ":" + mm + ":" + ss + " " + a);
   }, 1000);
